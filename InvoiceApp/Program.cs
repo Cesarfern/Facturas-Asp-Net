73ae08837878892c7fe3105d2f56e5292b0c8a74
César Fern <cesarfe@live.com.mx>
2026-04-27 01:46:03 -0600

    Fix redirect loop and update headers

--- InvoiceApp/Program.cs
+++ InvoiceApp/Program.cs
@@ -1,5 +1,6 @@
 using InvoiceApp.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     //app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 //app.UseHttpsRedirection();
 
