a1fdac2c88bf065c80d8f8bee16fefb0770c99a2
César Fern <cesarfe@live.com.mx>
2026-04-28 16:50:50 -0600

    agregar AppContext.SetSwitch

--- InvoiceApp/Program.cs
+++ InvoiceApp/Program.cs
@@ -2,6 +2,8 @@ using InvoiceApp.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpOverrides;
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("http://0.0.0.0:" + (Environment.GetEnvironmentVariable("PORT") ?? "80"));
